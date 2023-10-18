21f3911629ff203c7514a418ddf01b990b77cab0
MARUF3470 <smmaruf25@gmail.com>
2023-10-18 13:17:52 +0600

    pagination system added

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express')
-const multer = require('multer');
-const mongoose = require('mongoose');
-var cors = require('cors');
+const multer = require('multer'); // nodejs pakage for storing images
+const mongoose = require('mongoose'); // for connecting mongoose
+var cors = require('cors'); // to sent the data from frontent and get the data in frontent 
 require('dotenv').config()
 const { ObjectId } = require('mongodb');
 const app = express()
@@ -19,7 +19,7 @@ const connectDB = async () => {
         await mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true })
         console.log('database is connected');
     } catch (error) {
-        console.log('database is notconnected');
+        console.log('database is not-connected');
         console.log(error);
         process.exit(1)
     }
@@ -49,10 +49,7 @@ const productsSchema = new mongoose.Schema({
 
 })
 
-
 const Product = mongoose.model("products", productsSchema)
-//userName: FexiFurns
-//password: lpLNrX6ITIUFuWGy
 app.use('/uploads', express.static('uploads'));
 const storage = multer.diskStorage({
     destination: function (req, file, cb) {
@@ -71,7 +68,6 @@ const upload = multer({ storage: storage })
 //         }
 //     ]
 // });
-
 // const Image = mongoose.model('Image', imageSchema);
 
 app.post('/products', upload.array('images'), async (req, res) => {
@@ -86,14 +82,22 @@ app.post('/products', upload.array('images'), async (req, res) => {
     } catch (error) {
         res.status(500).json({ error: 'Internal server error' });
     }
-
 })
 app.get('/products', async (req, res) => {
     const page = parseInt(req.query.page);
     const size = parseInt(req.query.size);
+    const keyword = req.query.keyword
     try {
-        const count = await Product.estimatedDocumentCount()
-        const products = await Product.find().skip(page * size).limit(size)
+        let query = {};
+        if (keyword) {
+            query = { name: { $regex: new RegExp(keyword, 'i') } };
+            //Converting the keyword to a regular expression using RegExp is important because it allows you to perform a more flexible and powerful search, particularly when you want to match a pattern in a case-insensitive manner.
+            // using a regular expression allows you to construct patterns that provide more advanced matching options than simple string comparisons.
+            //The 'i' flag (stands for "ignore case") is one of several flags you can use with regular expressions.
+            // When you append 'i' to the regular expression, it tells the regular expression engine to perform a case -insensitive match
+        }
+        const count = await Product.countDocuments(query)
+        const products = await Product.find(query).skip(page * size).limit(size)
         res.json({ count, products });
     } catch (error) {
         console.error('Error fetching products:', error);
